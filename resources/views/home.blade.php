b41e79c1819e2b0d13c29b9f8d52702aa50474d5
Aidarchik <ishimgrad@gmail.com>
2021-11-10 15:26:28 +0500

    Соединение с БД, начало работы с шаблонами

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts')
+
+@section('main_content')
+	<div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequuntur sapiente, veniam molestias natus autem accusamus excepturi vel molestiae ad sunt recusandae quibusdam esse, rem non impedit voluptatum adipisci laudantium.</div>
+@endsection
